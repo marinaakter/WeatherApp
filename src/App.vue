740e516802b785e6fd03e9b7b6176a9892d7fa5e
Marina <marinaakter878@gmail.com>
2023-08-18 00:36:18 +0600

    Weather App

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary" >
+    <siteNavigation/>
+    <RouterView/>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import siteNavigation from './components/siteNavigation.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/BaseModel.vue
@@ -0,0 +1,31 @@
+<template>
+    <Transition>
+        <div
+        v-show="modelActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8" >
+            <Transition>
+                <div
+                v-if="modelActive"
+                class="p-4 bg-white self-start mt-32 max-w-screen-md ">
+                    <slot/>
+                    <button class="text-white mt-8 bg-weather-primary py-2 px-6 " @click="$emit('Close-modal')"
+                    >Close</button>
+                </div>
+            </Transition>
+        </div>
+    </Transition>
+</template>
+
+<script setup>
+defineEmits(["Close-modal"])
+defineProps({
+    modelActive: {
+        type: Boolean,
+        default: false,
+    }
+})
+</script>   
+
+<style lang="scss" scoped>
+
+</style>
