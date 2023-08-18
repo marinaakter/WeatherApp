5b233fd57e824e651252b98eb7556d8f2ac4a4cf
Marina <marinaakter878@gmail.com>
2023-08-18 22:41:19 +0600

    design changes2

--- src/components/BaseModel.vue
+++ src/components/BaseModel.vue
@@ -1,26 +1,67 @@
 <template>
-    <Transition>
-        <div v-show="modelActive"
-            class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30">
-            <Transition>
-                <div v-if="modelActive" class="self-start max-w-screen-md p-4 mt-32 bg-white rounded-lg">
-                    <slot />
-                    <button class="px-6 py-2 mt-8 text-white bg-gray-800 rounded-md"
-                        @click="$emit('Close-modal')">Close</button>
-                </div>
-            </Transition>
-        </div>
+  <Teleport to="body" >
+    <Transition name="modal-outer">
+      <div
+        v-show="modelActive"
+        class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modelActive"
+            class="self-start max-w-screen-md p-4 mt-32 bg-white rounded-lg"
+          >
+            <slot />
+            <button
+              class="px-6 py-2 mt-8 text-white bg-gray-800 rounded-md"
+              @click="$emit('Close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
     </Transition>
+  </Teleport>
 </template>
 
 <script setup>
-defineEmits(["Close-modal"])
+defineEmits(["Close-modal"]);
 defineProps({
-    modelActive: {
-        type: Boolean,
-        default: false,
-    }
-})
-</script>   
+  modelActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to{
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+
+.modal-inner-leave-active{
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from{
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to{
+  transform: scale(0.8);
+}
+
+
+</style>
