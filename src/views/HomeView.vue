f0fa55df9b81105031e1795eccf6979863246d47
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-08-25 11:13:44 +0600

    geo location integrated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,10 @@
 <template>
   <main class="container text-white">
-    <div class="pt-4 mb-8 relative">
-      <input
-        type="text"
-        v-model="searchQuery"
-        @input="getSearchResults"
-        placeholder="Search For a City or State"
-        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
-      />
-      <ul
-        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
-      >
-        <li
-          v-for="searchResult in mapboxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
+    <div class="relative pt-4 mb-8">
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search For a City or State"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
           {{ searchResult.place_name }}
         </li>
       </ul>
@@ -28,7 +17,7 @@ import axios from "axios";
 import { ref } from "vue";
 
 const mapboxAPIKEY =
-  "pk.eyJ11joiam9obmtvbWFybm1ja2kiLCJhIjoiY2t5NjFz0DZvMHJkaDJ1bWx60GVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
+  "pk.eyJ1IjoibmFzaW11bGhhc2FuIiwiYSI6ImNrYWR2N3FnYjBibDQyeG5sdmhnaDZkc2wifQ.3Us9I385DLrGnq9o8-et-A";
 
 const searchQuery = ref("");
 const queryTimeOut = ref(null);
@@ -39,7 +28,7 @@ const getSearchResults = () => {
   queryTimeOut.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
       const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKEY}&types=places`
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKEY}`
       );
       mapboxSearchResults.value = result.data.features;
       console.log(mapboxSearchResults.value);
