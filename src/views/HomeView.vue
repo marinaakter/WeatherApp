e55f5ea429cc12094e077519347c4347896e61b9
Nasimul Hasan <nasimulhasandeep@gmail.com>
2023-08-31 14:01:31 +0600

    city prblem solved

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,15 @@
 <template>
   <main class="container text-white">
     <div class="relative pt-4 mb-8">
-      <input
-        type="text"
-        v-model="searchQuery"
-        @input="getSearchResults"
-        placeholder="Search For a City or State"
-        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
-      />
-      <ul
-        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
-        v-if="mapboxSearchResults"
-      >
-      <p v-if="searchError" >Sorry, something went wrong, please try again.</p>
-      <p v-if="!serverError && mapboxSearchResults.length===0" > No results match you query, try a different term.</p>
-        <template v-else >
-          <li
-            v-for="searchResult in mapboxSearchResults"
-            :key="searchResult.id"
-            class="py-2 cursor-pointer"
-            @click="previewCity(searchResult)"
-          >
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search For a City or State"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!serverError && mapboxSearchResults.length === 0"> No results match you query, try a different term.</p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -28,7 +17,7 @@
     </div>
     <div class="flex flex-col gap-4">
       <Suspense>
-        <CityList/>
+        <CityList />
         <template #fallback>
           <p>Loading...</p>
         </template>
@@ -41,6 +30,7 @@
 import axios from "axios";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import CityList from "../components/CityList.vue";
 
 const router = useRouter();
 const previewCity = (searchResult) => {
@@ -49,8 +39,8 @@ const previewCity = (searchResult) => {
   router.push({
     name: "cityView",
     params: {
-      state:state.replaceAll(" ",""),city:city,
-      },
+      state: state.replaceAll(" ", ""), city: city,
+    },
     query: {
       lat: searchResult.geometry.coordinates[1],
       lng: searchResult.geometry.coordinates[0],
@@ -72,10 +62,10 @@ const getSearchResults = () => {
   queryTimeOut.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
       try {
-         const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKEY}`
-      );
-      mapboxSearchResults.value = result.data.features;
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKEY}`
+        );
+        mapboxSearchResults.value = result.data.features;
       } catch {
         searchError.value = true;
       }
